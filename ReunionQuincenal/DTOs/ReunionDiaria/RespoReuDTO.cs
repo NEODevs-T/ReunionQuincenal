53e97d0902b577098e3ef65cafce22df77a2c18b
Humberto7913 <humbertoalfonso8@gmail.com>
2025-11-05 14:21:40 -0400

    Se elimino el uso de MapResponsableKey para utlizar el campo traduccido de la base de datos

--- ReunionQuincenal/DTOs/ReunionDiaria/RespoReuDTO.cs
+++ ReunionQuincenal/DTOs/ReunionDiaria/RespoReuDTO.cs
@@ -9,6 +9,7 @@ public partial class RespoReuDTO
     public int IdResReu { get; set; }
 
     public string Rrnombre { get; set; } = null!;
+    public string Rrenglish { get; set; } 
 
     public bool Rresta { get; set; }
 
