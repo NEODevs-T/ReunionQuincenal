970dc03151f6f42ce4d9662ffc7196277ae1ac1a
Humberto7913 <humbertoalfonso8@gmail.com>
2026-03-10 15:46:35 -0400

    Se aumento el tamaño de la pageSize

--- ReunionQuincenal/Data/PizarraData.cs
+++ ReunionQuincenal/Data/PizarraData.cs
@@ -59,7 +59,7 @@ public class PizarraData : IPizarraData
         string idcentro, string iddiv, DateTime f1, DateTime f2, string tipo, string estado)
     {
         const int reunionQuincenal = 3;
-        const int pageSize = 5000; // único parámetro interno que se enviará
+        const int pageSize = 100000; // único parámetro interno que se enviará
 
         string f1Formateado = f1.ToString("yyyy-MM-dd");
         string f2Formateado = f2.ToString("yyyy-MM-dd");
